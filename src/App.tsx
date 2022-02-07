fbc767f71b44c10b68051686eeecfdac34f73f83
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-07 09:12:55 -0300

    ⭐ 💡 Feat(modal/init): inicializado a construção do Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Header } from './components/Header/index';
 import { Dashboard } from './components/Dashboard';
 
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
 export function App() {
+    const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsTransactionModalOpen(false);
+    }
+
     return (
         <>
-            <Header />
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
+            <Modal
+                isOpen={isTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            >
+                <h2>Cadastrar transação</h2>
+            </Modal>
             <GlobalStyle />
         </>
     );
